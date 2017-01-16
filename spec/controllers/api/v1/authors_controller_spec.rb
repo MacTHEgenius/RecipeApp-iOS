30554c2b2a9a873e13bbbc4d34b6800ad36be9a3
MacTHEgenius <levesque.benoit@me.com>
2017-01-16 18:13:38 -0500

    Refactor tests

--- spec/controllers/api/v1/authors_controller_spec.rb
+++ spec/controllers/api/v1/authors_controller_spec.rb
@@ -1,16 +1,19 @@
 require 'spec_helper'
 
 describe Api::V1::AuthorsController do
-  before(:each) { request.headers['Accept'] = "application/vnd.recipe_api.v1" }
+  before(:each) do
+    request.headers['Accept'] = "application/vnd.recipe_api.v1, #{Mime::JSON}"
+    request.headers['Content-Type'] = Mime::JSON.to_s
+  end
 
   describe "GET #show" do
     before(:each) do
       @author = FactoryGirl.create :author
-      get :show, id: @author.id, format: :json
+      get :show, id: @author.id
     end
 
     it "returns the information about a reporter on a hash" do
-      author_response = JSON.parse(response.body, symbolize_names: true)
+      author_response = JSON.parse(response.body, symbolize_names: true) # Replace each with json_response
       expect(author_response[:email]).to eql @author.email
     end
 
@@ -22,7 +25,7 @@ describe Api::V1::AuthorsController do
     context "when is successfully created" do
       before(:each) do
         @author_attributes = FactoryGirl.attributes_for :author
-        post :create, { author: @author_attributes }, format: :json
+        post :create, { author: @author_attributes }
       end
 
       it "renders the json representation for the author record just created" do
@@ -38,7 +41,7 @@ describe Api::V1::AuthorsController do
         # no email
         @invalid_author_attributes = { password: "12345678",
                                      password_confirmation: "12345678" }
-        post :create, { author: @invalid_author_attributes }, format: :json
+        post :create, { author: @invalid_author_attributes }
       end
 
       it "renders an errors json" do
@@ -61,7 +64,7 @@ describe Api::V1::AuthorsController do
       before(:each) do
         @author = FactoryGirl.create :author
         patch :update, { id: @author.id,
-                         author: { email: "newmail@example.com" } }, format: :json
+                         author: { email: "newmail@example.com" } }
       end
 
       it "renders the json representation for the updated author" do
@@ -76,7 +79,7 @@ describe Api::V1::AuthorsController do
       before(:each) do
         @author = FactoryGirl.create :author
         patch :update, { id: @author.id,
-                         author: { email: "bademail.com" } }, format: :json
+                         author: { email: "bademail.com" } }
       end
 
       it "renders an errors json" do
@@ -96,7 +99,7 @@ describe Api::V1::AuthorsController do
   describe "DELETE #destroy" do
     before(:each) do
       @author = FactoryGirl.create :author
-      delete :destroy, { id: @author.id }, format: :json
+      delete :destroy, { id: @author.id }
     end
 
     it { should respond_with 204 }
