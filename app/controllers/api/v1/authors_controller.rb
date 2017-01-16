2a45a077cfc1723fe470aa501aaf38c72df712d2
MacTHEgenius <levesque.benoit@me.com>
2017-01-16 17:21:47 -0500

    Add create (POST) author

--- app/controllers/api/v1/authors_controller.rb
+++ app/controllers/api/v1/authors_controller.rb
@@ -8,4 +8,20 @@ class Api::V1::AuthorsController < ApplicationController
     respond_with Author.find(params[:id])
   end
 
+  # POST
+
+  def create
+    author = Author.new(author_params)
+    if author.save
+      render json: author, status: 201, location: [:api, author]
+    else
+      render json: { errors: author.errors }, status: 422
+    end
+  end
+
+  private
+    def author_params
+      params.require(:author).permit(:email, :password, :password_confirmation)
+    end
+
 end
--- spec/controllers/api/v1/authors_controller_spec.rb
+++ spec/controllers/api/v1/authors_controller_spec.rb
@@ -16,4 +16,43 @@ describe Api::V1::AuthorsController do
 
     it { should respond_with 200 }
   end
+
+  describe "POST #create" do
+
+    context "when is successfully created" do
+      before(:each) do
+        @author_attributes = FactoryGirl.attributes_for :author
+        post :create, { author: @author_attributes }, format: :json
+      end
+
+      it "renders the json representation for the author record just created" do
+        author_response = JSON.parse(response.body, symbolize_names: true)
+        expect(author_response[:email]).to eql @author_attributes[:email]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "when is not created" do
+      before(:each) do
+        # no email
+        @invalid_author_attributes = { password: "12345678",
+                                     password_confirmation: "12345678" }
+        post :create, { author: @invalid_author_attributes }, format: :json
+      end
+
+      it "renders an errors json" do
+        author_response = JSON.parse(response.body, symbolize_names: true)
+        expect(author_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the author could not be created" do
+        author_response = JSON.parse(response.body, symbolize_names: true)
+        expect(author_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+
 end
