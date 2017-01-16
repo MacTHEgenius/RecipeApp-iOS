708168452a715dc5bbd467f5f67cade5e97be877
MacTHEgenius <levesque.benoit@me.com>
2017-01-16 15:33:51 -0500

    Add versionning

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # API definition
   namespace :api, defaults: { format: :json }, contraints: { subdomain: 'api' }, path: '/' do
 
+    scope module: :v1 do
+
+    end
+
   end
 
 end
