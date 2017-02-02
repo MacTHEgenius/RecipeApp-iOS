91473066e34575a866b8c93f0aa362a5351c7d55
MacTHEgenius <levesque.benoit@me.com>
2017-02-02 17:03:28 -0500

    Delete .idea

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::RecipesController < ApplicationController
 
+  before_action :authenticate_with_token!, only: [:create]
   respond_to :json
 
   # GET (all)
@@ -13,5 +14,16 @@ class Api::V1::RecipesController < ApplicationController
   def show
     respond_with Recipe.find(params[:id])
   end
+
+  # POST
+
+  def create
+    recipe = current_author.recipes.build()
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name, :cooking_duration, :baking_duration)
+    end
 
 end
--- spec/controllers/api/v1/recipes_controller_spec.rb
+++ spec/controllers/api/v1/recipes_controller_spec.rb
@@ -30,4 +30,44 @@ describe Api::V1::RecipesController do
     it { should respond_with 200 }
   end
 
+  describe "POST #create" do
+    context "when is successfully created" do
+      before(:each) do
+        author = FactoryGirl.create :author
+        @recipe_attributes = FactoryGirl.attributes_for :recipe
+        api_authorization_header author.auth_token
+        post :create, { author_id: author.id, recipe: @recipe_attributes }
+      end
+
+      it "renders the json representation fo the recipe record just created" do
+        recipe_response = JSON.parse(response.body, symbolize_names: true)
+        expect(recipe_response[:name]).to eql @recipe_attributes[:name]
+
+        it { should respond_with 201 }
+      end
+
+      context "when is not created" do
+        before(:each) do
+          author = FactoryGirl.create :author
+          @invalid_recipe_attributes = { name: "A recipe", cooking_duration: "a", baking_duration: "b" }
+          api_authorization_header author.auth_token
+          post :create, { author_id: author.id, recipe: @invalid_recipe_attributes }
+        end
+
+        it "renders an errors json" do
+          recipe_response = JSON.parse(response.body, symbolize_names: true)
+          expect(recipe_response).to have_key(:errors)
+        end
+
+        it "renders the json errors on why the author could not be created" do
+          recipe_response = JSON.parse(response.body, symbolize_names: true)
+          expect(recipe_response[:errors][:cooking_duration]).to include "is not a number"
+          expect(recipe_response[:errors][:baking_duration]).to include "is not a number"
+        end
+
+        it { should respond_with 422 }
+      end
+    end
+  end
+
 end
