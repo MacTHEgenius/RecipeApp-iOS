5a954510e5cdbcd7f3a70802a1f71b8f198f0b77
MacTHEgenius <levesque.benoit@me.com>
2017-03-18 22:50:14 -0400

    Filter json for recipe controller

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -6,13 +6,13 @@ class Api::V1::RecipesController < ApplicationController
   # GET (all)
 
   def index
-    respond_with Recipe.all
+    respond_with json(Recipe.all)
   end
 
   # GET (by id)
 
   def show
-    respond_with Recipe.find(params[:id])
+    respond_with json(Recipe.find(params[:id]))
   end
 
   # POST
@@ -24,6 +24,10 @@ class Api::V1::RecipesController < ApplicationController
   private
     def recipe_params
       params.require(:recipe).permit(:name, :cooking_duration, :baking_duration)
+    end
+
+    def json(obj)
+      obj.to_json(only: [:name, :cooking_duration, :baking_duration], include: { steps: { only: [:count, :description] }, ingredients: { only: [:quantity, :name] }})
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ require 'api_constraints'
 Rails.application.routes.draw do
 
   devise_for :authors
+
   # API definition
+
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
 
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
